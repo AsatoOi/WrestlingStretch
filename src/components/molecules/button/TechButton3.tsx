aa29af6101a4d1447cf460a2c1a87651a6f21298
AsatoOi <83337845+AsatoOi@users.noreply.github.com>
2022-01-24 20:58:52 +0900

    "[fix]eslintの設定"

--- src/components/molecules/button/TechButton3.tsx
+++ src/components/molecules/button/TechButton3.tsx
@@ -1,3 +1,4 @@
+/*eslint-disable react-hooks/exhaustive-deps*/
 import { memo, useCallback, VFC } from "react";
 import { Box, Flex, Text, Heading, Image } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
